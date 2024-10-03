2eb4aa0dbcd2f82d84e62e12bfa44755c4964f03
pharaohSamma <mohyisaadani001@gmail.com>
2024-10-03 13:33:32 +0100

    data fetching logic

---
+++ src/model/newsModel.js
@@ -0,0 +1,20 @@
+const newsModel = {
+    fetchNews: function (callback){
+        const xhr = new XMLHttpRequest();
+        const apiKey = '05e367da39364c7b8c15fa9b968ff4e2';
+        const url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=05e367da39364c7b8c15fa9b968ff4e2`;
+
+        xhr.open('GET', url , true);
+        xhr.onload = function (){
+            if (xhr.status === 200){
+                const data = JSON.parse(xhr.responseText);
+                callback(null, data.articles);
+            } else {
+                callback('Error fetching data', null)
+            }
+        };
+        xhr.send();
+    }
+};
+
+export default newsModel;
