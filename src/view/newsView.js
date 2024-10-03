83715886b7809a3a32733346f305557546e46221
pharaohSamma <mohyisaadani001@gmail.com>
2024-10-03 14:55:47 +0100

    adding news photos to the container

--- src/view/newsView.js
+++ src/view/newsView.js
@@ -7,8 +7,10 @@ const newsView = {
             const newsItem = document.createElement('div');
             newsItem.classList.add('news-item');
             newsItem.innerHTML = `
+            <img src= "${article.urlToImage}" alt="${article.title}"> 
             <h2>${article.title}</h2>
             <p>${article.description || 'No description available.'}</p>
+             <a href="${article.url}" target="_blank">Read more</a>
             `;
             container.appendChild(newsItem)
         });
