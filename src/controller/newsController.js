7e31ac5101a1559441c5eb121ec0c9bb63149b2a
pharaohSamma <mohyisaadani001@gmail.com>
2024-10-04 18:36:52 +0100

    adding pagination logic

--- src/controller/newsController.js
+++ src/controller/newsController.js
@@ -1,12 +1,40 @@
 import newsModel from "../model/newsModel.js";
 import newsView from "../view/newsView.js";
 
+let cureentPage = 1;
+let pageSize = 8;
 const newsController = {
-
+   
     init: function () {
-        newsModel.fetchNews(function (error, articles){
-            if(error){
-                console.log(error);
+        //Assigment A1
+        // newsModel.fetchNews(function (error, articles){
+        //     if(error){
+        //         console.log(error);
+        //     } else {
+        //         newsView.renderNews(articles);
+        //     }
+        // });
+
+        //Assigment A2
+        this.fetchPage(cureentPage);
+
+        document.getElementById('next-btn').addEventListener('click', ()=>{
+            cureentPage ++;
+            this.fetchPage(cureentPage);
+        })
+
+        document.getElementById('prev-btn').addEventListener('click', () =>{
+            if(cureentPage >1){
+                cureentPage --;
+                this.fetchPage(cureentPage);
+            }
+        })
+    },
+
+    fetchPage: function(page){
+        newsModel.fetchNews(page, pageSize, function (error, articles) {
+            if(error) {
+                console.error(error);
             } else {
                 newsView.renderNews(articles);
             }
--- src/model/newsModel.js
+++ src/model/newsModel.js
@@ -1,8 +1,13 @@
 const newsModel = {
-    fetchNews: function (callback){
+    fetchNews: function (page, pageSize, callback){
         const xhr = new XMLHttpRequest();
         const apiKey = '05e367da39364c7b8c15fa9b968ff4e2';
-        const url = `https://newsapi.org/v2/everything?q=tesla&from=2024-09-04&sortBy=publishedAt&apiKey=${apiKey}`;
+        //assigment A1
+        // const url = `https://newsapi.org/v2/everything?q=tesla&from=2024-09-04&sortBy=publishedAt&apiKey=${apiKey}`;
+
+        //assigment A2
+        const url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&sortBy=publishedAt&page=${page}
+                                        &pageSize=${pageSize}&apiKey=${apiKey}`;
 
         xhr.open('GET', url , true);
         xhr.onload = function (){
